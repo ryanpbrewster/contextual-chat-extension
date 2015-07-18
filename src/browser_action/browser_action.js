21991e0c6cc1f00d15e3d61cb6c7f70c62fd0c20
ryanpbrewster <RyanPBrewster@gmail.com>
2015-07-18 17:54:09 -0500

    Got contextual chat to work on a by-url basis
    Some restructuring of the data. The main object is a "chat", which exists
    on a specific URL (only one per URL, though I don't know how to enforce that).
    Each chat also has a thread id associated with it.
    
    A thread contains a bunch of messages.
    
    A message contains a name and the message text.

--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -1,24 +1,63 @@
-Firebase.enableLogging(true);
-var myDataRef = new Firebase('https://ryanpbrewster-messenger.firebaseio.com/');
-var messengesRef = myDataRef.child("messenges");
-
-messengesRef.on('child_added', function(snapshot) {
-  var message = snapshot.val();
-  displayChatMessage(message.name, message.text);
+chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+  var url = tabs[0].url;
+  console.log("Found url: " + url);
+  console.log("About to set up contextual chat for this page");
+  setupContextualChat(url);
 });
 
-function displayChatMessage(name, text) {
-  console.log(name + ": " + text);
-  $('<div/>').text(text).prepend($('<em/>').text(name+': ')).prependTo($('#messagesDiv'));
-};
+function setupContextualChat(url) {
+  console.log("Setting up contextual chat");
+  console.log("First step is to find out if there is an existing chat");
+  findOrMakeNewChat(url);
+}
 
-document.getElementById("messageInput").addEventListener("keydown", function(e) {
-  console.log(e);
-  console.log(e.keyCode);
-  if (e.keyCode == 13) {
-    var name = $('#nameInput').val();
-    var text = $('#messageInput').val();
-    messengesRef.push({name: name, text: text});
-    $('#messageInput').val('');
-  }
-});
+function findOrMakeNewChat(url) {
+  console.log("Checking if there is an existing chat at this page.");
+  Firebase.enableLogging(false);
+  var myDataRef = new Firebase('https://ryanpbrewster-messenger.firebaseio.com/');
+  console.log("Set up myDataRef: " + myDataRef);
+
+  var chatsRef = myDataRef.child("chats");
+  var threadsRef = myDataRef.child("threads");
+
+
+  chatsRef.orderByChild("url").equalTo(url).limitToFirst(1).once("value", function(snapshot) {
+    console.log("[RPB] Checking to see if anything exists");
+    if( snapshot.exists() ) {
+      snapshot.forEach(function(data) {
+        console.log("[RPB] Found existing chat: " + data.key() + ": " + data.val());
+        console.log("We should set up the existing thread (" + data.val().thread);
+        setupListeners(threadsRef.child(data.val().thread));
+      });
+    } else {
+      console.log("[RPB] No chats found for this page.");
+      var newThread = threadsRef.push({});
+      console.log("Made a new thread: " + newThread + " with key " + newThread.key());
+      chatsRef.push({url: url, thread: newThread.key()})
+      console.log("Added the thread to chats");
+      setupListeners(newThread);
+    }
+  });
+}
+
+function setupListeners(threadRef) {
+  console.log("Setting up listeners for thread " + threadRef.key());
+  var messagesRef = threadRef.child("messages");
+  messagesRef.on('child_added', function(snapshot) {
+    var message = snapshot.val();
+    displayChatMessage(message.name, message.text);
+  });
+
+  function displayChatMessage(name, text) {
+    $('<div/>').text(text).prepend($('<em/>').text(name+': ')).prependTo($('#messagesDiv'));
+  };
+
+  document.getElementById("messageInput").addEventListener("keydown", function(e) {
+    if (e.keyCode == 13) {
+      var name = $('#nameInput').val();
+      var text = $('#messageInput').val();
+      messagesRef.push({name: name, text: text});
+      $('#messageInput').val('');
+    }
+  });
+}
