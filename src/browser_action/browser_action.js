4c16d40303f7785dd081f997f49655a0531e2257
ryanpbrewster <RyanPBrewster@gmail.com>
2015-07-19 14:16:30 -0500

    Restructured so that users have thread permissions
    user_threads for the threads which a given user has access to
    
    thread_users for the users which have access to a given thread.
    
    Still need to modify the Firebase data rules so that they can only
    access the appropriate things

--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -2,48 +2,117 @@ chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
   var url = tabs[0].url;
   console.log("Found url: " + url);
   console.log("About to set up contextual chat for this page");
+
   setupContextualChat(url);
 });
 
 function setupContextualChat(url) {
   console.log("Setting up contextual chat");
-  console.log("First step is to find out if there is an existing chat");
-  findOrMakeNewChat(url);
+  var myDataRef = new Firebase("https://ryanpbrewster-context-chat.firebaseio.com");
+
+  console.log("Let's see if this user is logged in")
+  var usersRef = myDataRef.child("users");
+  var authData = myDataRef.getAuth();
+  if (authData) {
+    console.log("User is already authenticated: " + authData.uid + authData.provider);
+    findOrMakeNewChat(myDataRef, url, authData);
+  } else {
+    console.log("They're not logged in, let's do this")
+    authenticateUser(usersRef, function(newAuthData) {
+      findOrMakeNewChat(myDataRef, url, newAuthData);
+    });
+  }
 }
 
-function findOrMakeNewChat(url) {
-  console.log("Checking if there is an existing chat at this page.");
-  Firebase.enableLogging(false);
-  var myDataRef = new Firebase('https://ryanpbrewster-messenger.firebaseio.com/');
-  console.log("Set up myDataRef: " + myDataRef);
+function authenticateUser(usersRef, callback) {
+  console.log("Trying to log this user in")
 
-  var chatsRef = myDataRef.child("chats");
-  var threadsRef = myDataRef.child("threads");
+  console.log("Calling the FB auth popup");
+  usersRef.authWithPassword({
+    email    : "ryanpbrewster@gmail.com",
+    password : "foobarbaz"
+  }, function(error, authData) {
+    if (error) {
+      console.log("Login Failed!", error);
+    } else {
+      console.log("Authenticated successfully with payload:", authData);
+      usersRef.child(authData.uid).set({});
+      callback(authData);
+    }
+  });
 
-
-  chatsRef.orderByChild("url").equalTo(url).limitToFirst(1).once("value", function(snapshot) {
-    console.log("[RPB] Checking to see if anything exists");
-    if( snapshot.exists() ) {
-      snapshot.forEach(function(data) {
-        console.log("[RPB] Found existing chat: " + data.key() + ": " + data.val());
-        console.log("We should set up the existing thread (" + data.val().thread);
-        setupListeners(threadsRef.child(data.val().thread));
+  console.log("Setting up a listener for authentication");
+  usersRef.onAuth(function(authData) {
+    console.log("Authentication trigger went off")
+    if (authData) {
+      // save the user"s profile into Firebase so we can list users,
+      // use them in Security and Firebase Rules, and show profiles
+      console.log("The name we extracted from authData is " + getName(authData));
+      usersRef.child(authData.uid).update({
+        provider: authData.provider,
+        name: getName(authData)
       });
     } else {
-      console.log("[RPB] No chats found for this page.");
-      var newThread = threadsRef.push({});
-      console.log("Made a new thread: " + newThread + " with key " + newThread.key());
-      chatsRef.push({url: url, thread: newThread.key()})
-      console.log("Added the thread to chats");
-      setupListeners(newThread);
+      console.log("Somehow the onAuth trigger hit but there's no authData")
     }
   });
 }
 
-function setupListeners(threadRef) {
-  console.log("Setting up listeners for thread " + threadRef.key());
-  var messagesRef = threadRef.child("messages");
-  messagesRef.on('child_added', function(snapshot) {
+function getName(authData) {
+  switch(authData.provider) {
+    case 'password':
+      return authData.password.email.replace(/@.*/, '');
+     case 'twitter':
+       return authData.twitter.displayName;
+     case 'facebook':
+       return authData.facebook.displayName;
+  }
+}
+
+
+function findOrMakeNewChat(myDataRef, url, authData) {
+  console.log("Checking if there is an existing chat at this page.");
+  Firebase.enableLogging(false);
+  console.log("Set up myDataRef: " + myDataRef);
+
+  var threadsRef = myDataRef.child("threads");
+  var messagesRef = myDataRef.child("thread_messages");
+  var threadUsersRef = myDataRef.child("thread_users");
+  var userThreadsRef = myDataRef.child("user_threads");
+
+
+  threadsRef.orderByChild("url").equalTo(url).limitToFirst(1).once("value", function(snapshot) {
+    console.log("[RPB] Checking to see if anything exists");
+    if( snapshot.exists() ) {
+      console.log("Snapshot exists: " + snapshot.val())
+      snapshot.forEach(function(data) {
+        console.log("[RPB] Found existing chat: " + data.key() + ": " + data.val());
+        console.log("We should set up the existing thread (" + data.val().threadKey);
+        setupListeners(messagesRef.child(data.key()));
+        return true;
+      });
+    } else {
+      console.log("[RPB] No chats found for this page.");
+      var newThread = threadsRef.push({url: url});
+      console.log("Made a new thread: " + newThread + " with key " + newThread.key());
+      var newThreadMessagesRef = messagesRef.child(newThread.key());
+      newThreadMessagesRef.set({});
+      console.log("Added the thread to messages: " + newThreadMessagesRef);
+
+      userThreadsRef.child(authData.uid).push({threadKey: newThread.key(), role: "creator"});
+      console.log("Added this thread to the list of threads for user");
+
+      threadUsersRef.child(newThread.key()).push({userId: authData.uid, role: "creator"});
+      console.log("Added this user to the list of users for thread");
+
+      setupListeners(newThreadMessagesRef);
+    }
+  });
+}
+
+function setupListeners(threadMessagesRef) {
+  console.log("Setting up listeners for thread " + threadMessagesRef.key());
+  threadMessagesRef.limitToLast(100).on('child_added', function(snapshot) {
     var message = snapshot.val();
     displayChatMessage(message.name, message.text);
   });
@@ -56,7 +125,7 @@ function setupListeners(threadRef) {
     if (e.keyCode == 13) {
       var name = $('#nameInput').val();
       var text = $('#messageInput').val();
-      messagesRef.push({name: name, text: text});
+      threadMessagesRef.push({name: name, text: text});
       $('#messageInput').val('');
     }
   });
